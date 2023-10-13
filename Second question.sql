ffa2450b975c61a6bd66870fed0c17e31dba00fb
Blanka Toboláková <137218368+BlankaTob@users.noreply.github.com>
2023-10-13 13:30:14 +0200

    Simplification of SQL Query for Calculating Average Values
    Description of Changes:
    - Reduction of Duplicate Code: The original query contained duplicated sections for different products and years, which were replaced with a single one using filters.
    - Use of Common Table Expression (CTE): Created a CTE named AverageData to store average data for salaries and food prices for the years 2008 and 2018.
    - Data Filtering: Added filters to select only specific years (2008 and 2018) and food categories ('Mléko polotučné pasterované' and 'Chléb konzumní kmínový').
    - Utilization of LEFT JOIN: Used LEFT JOIN to preserve all records from the main table and to ensure the correct average values from the CTE.
    
    This change significantly simplified the SQL query, reduced code duplication, and improved the query's readability and efficiency.

--- Second question.sql
+++ Second question.sql
@@ -1,99 +1,38 @@
-SELECT DISTINCT
-    btpt.year,
-    asalary.avg_salary_2008,
-    'mléko' AS product,
-    amilk.avg_milk_price_2008,
-    ROUND(asalary.avg_salary_2008 / amilk.avg_milk_price_2008, 2) AS quantity
-FROM
-    t_Blanka_Toboláková_project_SQL_primary_final AS btpt
-JOIN (
+WITH AverageData AS (
     SELECT
         YEAR,
-        ROUND(AVG(payroll_value), 2) AS avg_salary_2008
+        ROUND(AVG(CASE WHEN year = 2008 THEN payroll_value END), 2) AS avg_salary_2008,
+        ROUND(AVG(CASE WHEN year = 2008 AND food_category = 'Mléko polotučné pasterované' THEN avg_value_food END), 2) AS avg_milk_price_2008,
+        ROUND(AVG(CASE WHEN year = 2018 THEN payroll_value END), 2) AS avg_salary_2018,
+        ROUND(AVG(CASE WHEN year = 2018 AND food_category = 'Mléko polotučné pasterované' THEN avg_value_food END), 2) AS avg_milk_price_2018,
+        ROUND(AVG(CASE WHEN year = 2008 AND food_category = 'Chléb konzumní kmínový' THEN avg_value_food END), 2) AS avg_bread_price_2008,
+        ROUND(AVG(CASE WHEN year = 2018 AND food_category = 'Chléb konzumní kmínový' THEN avg_value_food END), 2) AS avg_bread_price_2018
     FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE year = 2008
+    WHERE year IN (2008, 2018)
     GROUP BY YEAR
-) AS asalary ON btpt.year = asalary.YEAR
-JOIN (
-    SELECT 
-        YEAR,
-        ROUND(AVG(avg_value_food), 2) AS avg_milk_price_2008
-    FROM t_BlankaT_primary_table
-    WHERE food_category = 'Mléko polotučné pasterované'
-    GROUP BY YEAR
-) AS amilk ON btpt.year = amilk.YEAR
-UNION 
+)
 SELECT DISTINCT
     btpt.year,
-    bsalary.avg_salary_2018,
     'mléko' AS product,
-    bmilk.avg_milk_price_2018,
-    ROUND(bsalary.avg_salary_2018 / bmilk.avg_milk_price_2018, 2) AS milk_quantity
-FROM
-    t_Blanka_Toboláková_project_SQL_primary_final AS btpt
-JOIN (
-    SELECT
-        YEAR,
-        ROUND(AVG(payroll_value), 2) AS avg_salary_2018
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE year = 2018
-    GROUP BY YEAR
-) AS bsalary ON btpt.year = bsalary.YEAR
-JOIN (
-    SELECT 
-        YEAR,
-        ROUND(AVG(avg_value_food), 2) AS avg_milk_price_2018
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE food_category = 'Mléko polotučné pasterované'
-    GROUP BY YEAR
-) AS bmilk ON btpt.year = bmilk.YEAR
+    COALESCE(asalary.avg_salary_2008, bsalary.avg_salary_2018) AS avg_salary,
+    COALESCE(amilk.avg_milk_price_2008, bmilk.avg_milk_price_2018) AS avg_milk_price,
+    ROUND(COALESCE(asalary.avg_salary_2008, bsalary.avg_salary_2018) / COALESCE(amilk.avg_milk_price_2008, bmilk.avg_milk_price_2018), 2) AS quantity
+FROM t_Blanka_Toboláková_project_SQL_primary_final AS btpt
+LEFT JOIN AverageData AS asalary ON btpt.year = asalary.YEAR AND asalary.YEAR = 2008
+LEFT JOIN AverageData AS bsalary ON btpt.year = bsalary.YEAR AND bsalary.YEAR = 2018
+LEFT JOIN AverageData AS amilk ON btpt.year = amilk.YEAR AND amilk.YEAR = 2008 AND btpt.food_category = 'Mléko polotučné pasterované'
+LEFT JOIN AverageData AS bmilk ON btpt.year = bmilk.YEAR AND bmilk.YEAR = 2018 AND btpt.food_category = 'Mléko polotučné pasterované'
+WHERE btpt.year IN (2008, 2018) AND btpt.food_category = 'Mléko polotučné pasterované'
 UNION
 SELECT DISTINCT
     btpt.year,
-    asalary.avg_salary_2008,
     'chléb' AS product,
-    abread.avg_bread_price_2008,
-    ROUND(asalary.avg_salary_2008 / abread.avg_bread_price_2008, 2) AS quantity
-FROM
-    t_Blanka_Toboláková_project_SQL_primary_final AS btpt
-JOIN (
-    SELECT
-        YEAR,
-        ROUND(AVG(payroll_value), 2) AS avg_salary_2008
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE year = 2008
-    GROUP BY YEAR
-) AS asalary ON btpt.year = asalary.YEAR
-JOIN (
-    SELECT 
-        YEAR,
-        ROUND(AVG(avg_value_food), 2) AS avg_bread_price_2008
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE food_category = 'Chléb konzumní kmínový'
-    GROUP BY YEAR
-) AS abread ON btpt.year = abread.YEAR
-UNION 
-SELECT DISTINCT
-    btpt.year,
-    bsalary.avg_salary_2018,
-    'chléb' AS product,
-    bbread.avg_bread_price_2018,
-    ROUND(bsalary.avg_salary_2018 / bbread.avg_bread_price_2018, 2) AS quantity
-FROM
-    t_Blanka_Toboláková_project_SQL_primary_final AS btpt
-JOIN (
-    SELECT
-        YEAR,
-        ROUND(AVG(payroll_value), 2) AS avg_salary_2018
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE year = 2018
-    GROUP BY YEAR
-) AS bsalary ON btpt.year = bsalary.YEAR
-JOIN (
-    SELECT 
-        YEAR,
-        ROUND(AVG(avg_value_food), 2) AS avg_bread_price_2018
-    FROM t_Blanka_Toboláková_project_SQL_primary_final
-    WHERE food_category = 'Chléb konzumní kmínový'
-    GROUP BY YEAR
-) AS bbread ON btpt.year = bbread.YEAR;
+    COALESCE(asalary.avg_salary_2008, bsalary.avg_salary_2018) AS avg_salary,
+    COALESCE(abread.avg_bread_price_2008, bbread.avg_bread_price_2018) AS avg_bread_price,
+    ROUND(COALESCE(asalary.avg_salary_2008, bsalary.avg_salary_2018) / COALESCE(abread.avg_bread_price_2008, bbread.avg_bread_price_2018), 2) AS quantity
+FROM t_Blanka_Toboláková_project_SQL_primary_final AS btpt
+LEFT JOIN AverageData AS asalary ON btpt.year = asalary.YEAR AND asalary.YEAR = 2008
+LEFT JOIN AverageData AS bsalary ON btpt.year = bsalary.YEAR AND bsalary.YEAR = 2018
+LEFT JOIN AverageData AS abread ON btpt.year = abread.YEAR AND abread.YEAR = 2008 AND btpt.food_category = 'Chléb konzumní kmínový'
+LEFT JOIN AverageData AS bbread ON btpt.year = bbread.YEAR AND bbread.YEAR = 2018 AND btpt.food_category = 'Chléb konzumní kmínový'
+WHERE btpt.year IN (2008, 2018) AND btpt.food_category = 'Chléb konzumní kmínový';
